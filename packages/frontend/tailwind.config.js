59df0266ac98e7f8e9291d2e74cc023c2ab89f28
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-08-09 08:35:58 +0200

    frontend: minor tailwind.config adjustment

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: [`./src/pages/**/*.{js,ts,jsx,tsx}`, `./src/components/**/*.{js,ts,jsx,tsx}`],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
