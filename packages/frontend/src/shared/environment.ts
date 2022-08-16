655f87aa418e85835dec6ef2c1de750a3e9a8991
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-08-16 16:09:40 +0200

    frontend: sort/rename rpc envs for clarity

--- packages/frontend/src/shared/environment.ts
+++ packages/frontend/src/shared/environment.ts
@@ -12,8 +12,11 @@ export const env = {
 
   rpcUrls: {
     1337: process.env.NEXT_PUBLIC_RPC_1337!, // Hardhat
-    1: process.env.NEXT_PUBLIC_RPC_1!, // Mainnet
+
+    1: process.env.NEXT_PUBLIC_RPC_1!, // Ethereum Mainnet
     5: process.env.NEXT_PUBLIC_RPC_5!, // Goerli
-    80001: process.env.NEXT_PUBLIC_RPC_80001!, // Polygon Mumbai
+
+    137: process.env.NEXT_PUBLIC_RPC_137!, // Polygon Mainnet
+    80001: process.env.NEXT_PUBLIC_RPC_80001!, // Mumbai
   },
 }
