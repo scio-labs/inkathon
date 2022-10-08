2ccc7e2644014ac9d06cb04747dfa894b5ef713a
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-10-08 10:51:20 +0200

    chore: read deployments json-files from contracts/ package

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -4,11 +4,13 @@ import { HardhatExport } from 'src/types/hardhat'
 /**
  * Dynamically aggregating all deployments (addresses, abis)
  */
+
 export type DeploymentsType = { [_: number]: Promise<HardhatExport> }
+
 export const deployments: DeploymentsType = env.supportedChains.reduce(
   (acc: DeploymentsType, chainId: number) => ({
     ...acc,
-    [chainId]: import(`src/deployments/${chainId}.json`),
+    [chainId]: import(`@ethathon/contracts/deployments/${chainId}.json`),
   }),
   {}
 )
