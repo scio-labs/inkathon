be6cb9a2db49d60a8499427a1a88df3c04e44674
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-11-16 08:22:59 +0100

    contracts: add abis to version-control

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -1,6 +1,6 @@
 export const deployments = {
   greeter: {
-    abi: import('@inkathon/contracts/greeter/target/ink/metadata.json'),
+    abi: import('@inkathon/contracts/greeter/abi/metadata.json'),
     address: '5CbJV57ky3XyPi9F9rU69KYU3wNHRE3BixFDLSrgurkWQTB4',
     // address: '5H8rGukb2DiVwXpcQ68FaNuyQHVm8tgCS6H3n71njwWiMvHu',
   },
