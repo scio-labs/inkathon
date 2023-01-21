bd45c15d0c816ddec05a5037126921e68b0ce559
Dennis Zoma <dennis@scio.xyz>
2023-01-21 12:45:40 +0100

    feat: Update use-inkathon & use new contract query/tx helpers

--- packages/frontend/src/components/web3/GreeterContractInteractions.tsx
+++ packages/frontend/src/components/web3/GreeterContractInteractions.tsx
@@ -1,6 +1,12 @@
 import { Button, Card, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react'
 import { ContractIds } from '@deployments/deployments'
-import { getGasLimit, useInkathon, useRegisteredContract } from '@scio-labs/use-inkathon'
+import {
+  contractQuery,
+  contractTx,
+  unwrapResultOrError,
+  useInkathon,
+  useRegisteredContract,
+} from '@scio-labs/use-inkathon'
 import { FC, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
@@ -19,14 +25,8 @@ export const GreeterContractInteractions: FC = () => {
     if (!contract || !api) return
     setFetchIsLoading(true)
     try {
-      const gasLimit = getGasLimit(api, '10000000000', '10000000000')
-      const { result, output } = await contract.query.greet('', {
-        gasLimit,
-      })
-      if (!result?.isOk || !output) {
-        throw new Error(result.toString())
-      }
-      const message = (output.toPrimitive() as { ok: string }).ok
+      const result = await contractQuery(api, '', contract, 'greet')
+      const message = unwrapResultOrError<string>(result)
       setGreeterMessage(message)
     } catch (e) {
       console.error(e)
@@ -51,21 +51,10 @@ export const GreeterContractInteractions: FC = () => {
     try {
       // Gather form value
       const newMessage = form.getValues('newMessage')
-
-      // Estimate gas
-      const gasLimit = getGasLimit(api, '10000000000', '10000000000')
-      const { gasRequired } = await contract.query.setMessage(
-        account.address,
-        { storageDepositLimit: null, gasLimit },
-        newMessage,
-      )
-
-      // Execute transaction
-      await contract.tx
-        .setMessage({ gasLimit: gasRequired }, newMessage)
-        .signAndSend(account.address, (result) => {
-          if (result?.status?.isInBlock) fetchGreeting()
-        })
+      // Estimate gas & send transaction
+      await contractTx(api, account.address, contract, 'setMessage', {}, [newMessage], (result) => {
+        if (result?.status?.isInBlock) fetchGreeting()
+      })
       toast.success(`Successfully updated greeting`)
     } catch (e) {
       console.error(e)
