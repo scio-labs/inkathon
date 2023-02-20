8665ed95d3cc43de93e1a0ee3e1268f66fb2f9be
Dennis Zoma <dennis@scio.xyz>
2023-02-20 14:39:38 +0100

    feat: Add telegram link & minor visual improvements

--- packages/frontend/src/components/home/HomePageTitle.tsx
+++ packages/frontend/src/components/home/HomePageTitle.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import githubCircleIcon from 'public/brand/inkathon-logo.png'
+import inkathonLogo from 'public/brand/inkathon-logo.png'
 import githubIcon from 'public/icons/github-button.svg'
 import sponsorIcon from 'public/icons/sponsor-button.svg'
+import telegramIcon from 'public/icons/telegram-button.svg'
 import vercelIcon from 'public/icons/vercel-button.svg'
 import { FC } from 'react'
 import 'twin.macro'
+import tw, { styled } from 'twin.macro'
+
+const StyledIconLink = styled(Link)(() => [
+  tw`opacity-90 transition-all hover:(-translate-y-0.5 opacity-100)`,
+])
 
 export const HomePageTitle: FC = () => {
   const title = 'ink!athon'
@@ -13,24 +19,24 @@ export const HomePageTitle: FC = () => {
   const githubHref = 'https://github.com/scio-labs/inkathon'
   const deployHref = 'https://github.com/scio-labs/inkathon#deployment'
   const sponsorHref = 'mailto:hello@scio.xyz'
+  const telegramHref = 'https://t.me/inkathon'
 
   return (
     <>
       <div tw="flex flex-col items-center text-center font-mono">
+        {/* Logo & Title */}
         <Link
           href={githubHref}
           target="_blank"
           className="group"
-          tw="flex cursor-pointer flex-col items-center"
+          tw="flex cursor-pointer items-center gap-4 rounded-3xl border-2 border-gray-900 py-1.5 px-3.5 transition-all hover:(border-gray-300 bg-gray-900)"
         >
-          {/* Github Icon */}
-          <Image src={githubCircleIcon} priority width={110} alt="ink!athon Logo" />
-          {/* Title */}
-          <h1 tw="mt-4 font-black text-4xl tracking-tight">{title}</h1>
+          <Image src={inkathonLogo} priority width={60} alt="ink!athon Logo" />
+          <h1 tw="font-black text-[2.5rem]">{title}</h1>
         </Link>
 
         {/* Tagline & Links */}
-        <p tw="mt-1 text-gray-600 text-sm">
+        <p tw="mt-2 text-gray-600 text-sm">
           By{' '}
           <a
             href="https://zoma.dev"
@@ -52,16 +58,20 @@ export const HomePageTitle: FC = () => {
 
         {/* Github & Vercel Buttons */}
         <div tw="flex space-x-2">
-          <Link tw="opacity-80 hover:opacity-100" href={githubHref} target="_blank">
+          <StyledIconLink href={githubHref} target="_blank">
             <Image src={githubIcon} priority height={32} alt="Github Repository" />
-          </Link>
-          <Link tw="opacity-80 hover:opacity-100" href={deployHref} target="_blank">
+          </StyledIconLink>
+          <StyledIconLink href={deployHref} target="_blank">
             <Image src={vercelIcon} priority height={32} alt="Deploy with Vercel" />
-          </Link>
-          <Link tw="opacity-80 hover:opacity-100" href={sponsorHref} target="_blank">
+          </StyledIconLink>
+          <StyledIconLink href={telegramHref} target="_blank">
+            <Image src={telegramIcon} priority height={32} alt="Telegram Group" />
+          </StyledIconLink>
+          <StyledIconLink href={sponsorHref} target="_blank">
             <Image src={sponsorIcon} priority height={32} alt="Sponsor the Project" />
-          </Link>
+          </StyledIconLink>
         </div>
+
         <div tw="my-14 w-14 bg-gray-800 h-[2px]" />
       </div>
     </>
