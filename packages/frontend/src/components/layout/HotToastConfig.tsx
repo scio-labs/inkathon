c3cd21bcd1804d13ec3acc0afcf94e499e630094
Dennis Zoma <dennis@scio.xyz>
2023-02-21 08:38:43 +0100

    feat: Improve positioning of multiple toasts

--- packages/frontend/src/components/layout/HotToastConfig.tsx
+++ packages/frontend/src/components/layout/HotToastConfig.tsx
@@ -4,10 +4,12 @@ import { Toaster } from 'react-hot-toast'
 export const HotToastConfig: FC = () => {
   return (
     <Toaster
+      containerStyle={{
+        top: 60,
+      }}
       toastOptions={{
         position: 'top-center',
         style: {
-          marginTop: '2rem',
           wordBreak: 'break-all',
           maxWidth: '30rem',
           background: '#1a1b1f',
