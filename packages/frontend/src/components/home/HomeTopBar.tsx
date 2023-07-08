19338dc2e5154fe5b8d5987035d88b998da09793
Dennis Zoma <dennis@scio.xyz>
2023-07-08 14:04:23 +0200

    feat: Add `useInkathon` updated notice

--- packages/frontend/src/components/home/HomeTopBar.tsx
+++ packages/frontend/src/components/home/HomeTopBar.tsx
@@ -10,9 +10,11 @@ export const HomeTopBar: FC = () => {
         href="https://github.com/scio-labs/use-inkathon"
         tw="absolute top-0 left-0 right-0 z-10 flex items-center justify-center whitespace-pre-wrap bg-gray-900 py-3 px-2 text-center font-semibold text-sm text-white/75 hover:text-white"
       >
-        <div tw="mr-2 rounded bg-amber-300 px-1 py-0.5 font-black text-xs text-black">NEW</div>
+        <div tw="mr-2 rounded bg-fuchsia-400 px-1.5 py-0.5 font-bold text-xs text-black">
+          UPDATED
+        </div>
         <div tw="font-bold">
-          <span tw="hidden sm:inline">We released our new </span>React Hooks library `useInkathon`
+          <span tw="hidden sm:inline">Check out the our </span>React Hooks library `useInkathon`
         </div>
         <HiOutlineExternalLink tw="ml-1.5" />
       </Link>
