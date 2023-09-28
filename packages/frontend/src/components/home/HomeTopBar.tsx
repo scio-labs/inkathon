647e221a95edd5d9b4db9f825f4507e9a32623b9
Dennis Zoma <dennis@scio.xyz>
2023-09-28 19:01:02 +0200

    feat: Update `HomeTopBar` content to link to workshop video

--- packages/frontend/src/components/home/HomeTopBar.tsx
+++ packages/frontend/src/components/home/HomeTopBar.tsx
@@ -7,14 +7,15 @@ export const HomeTopBar: FC = () => {
   return (
     <>
       <Link
-        href="https://github.com/scio-labs/use-inkathon"
+        href="https://www.youtube.com/watch?v=SoNLZfsd0mQ"
         tw="absolute top-0 left-0 right-0 z-10 flex items-center justify-center whitespace-pre-wrap bg-gray-900 py-3 px-2 text-center font-semibold text-sm text-white/75 hover:text-white"
       >
-        <div tw="mr-2 rounded bg-fuchsia-400 px-1.5 py-0.5 font-bold text-xs text-black">
-          UPDATED
+        <div tw="mr-2 rounded px-1.5 py-0.5 font-bold text-xs text-white bg-[#ee391c]">
+          <span tw="mr-1 hidden sm:inline">VIDEO</span>▶
         </div>
         <div tw="font-bold">
-          <span tw="hidden sm:inline">Check out the our </span>React Hooks library `useInkathon`
+          <span tw="hidden sm:inline">Watch the sub0 ink!athon workshop (45 min)</span>
+          <span tw="inline sm:hidden">sub0 ink!athon workshop</span>
         </div>
         <HiOutlineExternalLink tw="ml-1.5" />
       </Link>
