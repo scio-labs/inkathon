e9edd27408400e2ba78230411ab7573f340219cb
Dennis Zoma <dennis@scio.xyz>
2023-10-13 23:41:20 +0200

    fix: Second try fixing module import error

--- frontend/src/deployments/deployments.ts
+++ frontend/src/deployments/deployments.ts
@@ -12,15 +12,14 @@ export const getDeployments = async (): Promise<SubstrateDeployment[]> => {
   const networks = env.supportedChains
   const deployments: SubstrateDeployment[] = []
 
-  for (const network of networks) {
+  for (const networkId of networks) {
     for (const contractId of Object.values(ContractIds)) {
-      deployments.push({
-        contractId: ContractIds.Greeter,
-        networkId: network,
-        abi: await import(`@inkathon/contracts/deployments/${contractId}/${contractId}.json`),
-        address: (await import(`@inkathon/contracts/deployments/${contractId}/${network}.ts`))
-          .address,
-      })
+      const abi = await import(`@inkathon/contracts/deployments/${contractId}/${contractId}.json`)
+      const { address } = await import(
+        `@inkathon/contracts/deployments/${contractId}/${networkId}.ts`
+      )
+
+      deployments.push({ contractId, networkId, abi, address })
     }
   }
 
