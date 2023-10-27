51fbbd53a0bbf6fae68e85ba41f54ccaef671ac4
Dennis Zoma <dennis@scio.xyz>
2023-10-27 13:17:44 +0200

    fix: `@polkadot/*` package duplication warnings (improvement)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,18 +6,10 @@
  * @type {import('next').NextConfig}
  **/
 let nextConfig = {
-  // Fix for warnings about cjs/esm package duplication
-  // See: https://github.com/polkadot-js/api/issues/5636
   transpilePackages: [
-    '@polkadot/api',
-    '@polkadot/api-contract',
-    '@polkadot/extension-dapp',
-    '@polkadot/extension-inject',
-    '@polkadot/keyring',
-    '@polkadot/types',
-    '@polkadot/types-support',
-    '@polkadot/util',
-    '@polkadot/util-crypto',
+    // Fix for warnings about cjs/esm package duplication
+    // See: https://github.com/polkadot-js/api/issues/5636
+    '**@polkadot/**',
   ],
 }
 
