14eee12256c3c6966a5a51f5a6d8db11a5da719b
ical10 <mochhusnir@gmail.com>
2023-11-02 19:26:18 +0700

    feat: Minor styling improvements

--- frontend/src/app/home/HomeTopBar.tsx
+++ frontend/src/app/home/HomeTopBar.tsx
@@ -9,7 +9,7 @@ export const HomeTopBar: FC = () => {
     <>
       <Link
         href="https://www.youtube.com/watch?v=SoNLZfsd0mQ"
-        className="absolute left-0 right-0 top-0 z-10 flex items-center justify-center whitespace-pre-wrap bg-gray-900 px-2 py-3 text-center text-sm font-semibold text-white/75 hover:text-white"
+        className="absolute left-0 right-0 top-0 z-10 flex items-center justify-center whitespace-pre-wrap bg-zinc-900 px-2 py-3 text-center text-sm font-semibold text-white/75 hover:text-white"
       >
         <div className="mr-2 rounded bg-[#ee391c] px-1.5 py-0.5 text-xs font-bold text-white">
           <span className="mr-1 hidden sm:inline">VIDEO</span>▶
