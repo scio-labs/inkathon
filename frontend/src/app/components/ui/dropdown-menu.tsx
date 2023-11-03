27cd6e8db2ef94d5f17937bc5baadd46891f92f8
ical10 <mochhusnir@gmail.com>
2023-11-03 15:15:27 +0700

    feat: Merge utils dir

--- frontend/src/app/components/ui/dropdown-menu.tsx
+++ frontend/src/app/components/ui/dropdown-menu.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import * as React from 'react'
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { Check, ChevronRight, Circle } from 'lucide-react'
+import * as React from 'react'
 
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils/cn'
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
@@ -168,18 +168,18 @@ DropdownMenuShortcut.displayName = 'DropdownMenuShortcut'
 
 export {
   DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
-  DropdownMenuGroup,
-  DropdownMenuPortal,
   DropdownMenuSub,
   DropdownMenuSubContent,
   DropdownMenuSubTrigger,
-  DropdownMenuRadioGroup,
+  DropdownMenuTrigger,
 }
--- frontend/src/app/web3/connect-button.tsx
+++ frontend/src/app/web3/connect-button.tsx
@@ -10,8 +10,8 @@ import {
   DropdownMenuTrigger,
 } from '@/app/components/ui/dropdown-menu'
 import { env } from '@/config/environment'
-import { truncateHash } from '@/utils/truncate-hash'
-import { useIsSSR } from '@/utils/use-is-ssr'
+import { truncateHash } from '@/lib/utils/truncate-hash'
+import { useIsSSR } from '@/lib/utils/use-is-ssr'
 import { SupportedChainId } from '@azns/resolver-core'
 import { useResolveAddressToDomain } from '@azns/resolver-react'
 import { InjectedAccount } from '@polkadot/extension-inject/types'
