35edaba050418f707081d70c19a514fca1da4e13
Dennis Zoma <dennis@scio.xyz>
2025-08-04 15:33:26 +0200

    feat: Update site metadata & image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next"
 import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { siteMetadata } from "@/lib/metadata"
 import { cn } from "../lib/utils"
 import { fontStyles } from "../styles/fonts"
 
 import "../styles/globals.css"
 
-export const metadata: Metadata = {
-  title: "inkathon Boilerplate",
-  description:
-    "Next generation full-stack boilerplate for ink! smart contracts running on PolkaVM. Powered by Papi, ReactiveDOT, Pop CLI, and more.",
-}
+export const metadata: Metadata = siteMetadata
 
 export default function RootLayout({
   children,
---
+++ frontend/src/lib/metadata.ts
@@ -0,0 +1,31 @@
+import type { Metadata } from "next"
+
+export const siteTitle = "inkathon Boilerplate"
+export const siteDescription =
+  "Next generation full-stack boilerplate for ink! smart contracts running on PolkaVM. Powered by Papi, ReactiveDOT, Pop CLI, and more."
+
+export const siteBanner = {
+  url: "/inkathon-banner.jpg",
+  width: 1200,
+  height: 630,
+  alt: "inkathon Boilerplate",
+}
+
+export const siteMetadata: Metadata = {
+  title: siteTitle,
+  description: siteDescription,
+  openGraph: {
+    images: [siteBanner],
+  },
+  twitter: {
+    images: [siteBanner],
+    description: siteDescription,
+    card: "summary_large_image",
+    site: "@scio_xyz",
+    creator: "@scio_xyz",
+  },
+  authors: [
+    { name: "Scio Labs", url: "https://scio.xyz" },
+    { name: "Dennis Zoma", url: "https://zoma.dev" },
+  ],
+}
