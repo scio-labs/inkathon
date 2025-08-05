4343204851b9f53814da2101ed9c519fe195a618
Dennis Zoma <dennis@scio.xyz>
2025-08-05 21:53:31 +0200

    chore(frontend): Add logo alt

--- frontend/src/components/layout/logo.tsx
+++ frontend/src/components/layout/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import logoSrc from "@/public/inkathon-logo--white.svg"
+import logoSrc from "@/public/inkathon-logo-with-version--white.svg"
 
 export function Logo() {
   return (
